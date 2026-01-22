6e0f1c6cd19a7660c52bff02199eefe32bb342f6
shahvirb <shahvirb@gmail.com>
2026-01-21 21:15:13 -0600

    completed prototype for beacon

--- conf/apps/beacon.py
+++ conf/apps/beacon.py
@@ -13,9 +13,25 @@ class Beacon(Hass):
             password=self.args.get("password")
         )
 
-        self.run_every(self.do, "now", 5)
+        self.run_every(self.do, "now", 3)
 
     def do(self, kwargs):
         monitors = self.uk.api.get_monitors()
-        self.log(f"Uptime Kuma Monitors: {len(monitors)} monitors found")
-        self.turn_on(self.indicator_light, rgb_color=[255, 0, 0])
+        # self.log(f"Uptime Kuma Monitors: {len(monitors)} monitors found")
+        # page = self.uk.api.get_status_page("beacon")
+        # self.log(f"Uptime Kuma Status Page: {page['publicGroupList']}")
+
+        monitors = self.uk.get_relevant_monitors()
+        up_count = sum(1 for m in monitors if m.get('status') == 'up')
+        total_count = len(monitors)
+        self.log(f"uptime kuma monitors: {up_count}/{total_count} are up")
+
+        percentage = up_count / total_count if total_count > 0 else 0
+        if percentage == 1.0:
+            color_name = "green"
+        elif percentage == 0.0:
+            color_name = "red"
+        else:
+            color_name = "yellow"
+
+        self.turn_on(self.indicator_light, color_name=color_name)
--- conf/apps/uptimekuma.py
+++ conf/apps/uptimekuma.py
@@ -1,8 +1,73 @@
 import logging
 from uptime_kuma_api import UptimeKumaApi
+from uptime_kuma_api import MonitorStatus
+
+class StatusPageParser:
+    def __init__(self, status_page_data):
+        self.data = status_page_data
+    
+    def get_groups(self):
+        """Returns list of all groups with their metadata."""
+        return [{'id': g['id'], 'name': g['name'], 'weight': g['weight']} 
+                for g in self.data]
+    
+    def get_monitors(self, group_id=None):
+        """Returns monitors, optionally filtered by group_id."""
+        monitors = []
+        for group in self.data:
+            if group_id is None or group['id'] == group_id:
+                for monitor in group.get('monitorList', []):
+                    monitors.append({
+                        'group_id': group['id'],
+                        'group_name': group['name'],
+                        **monitor
+                    })
+        return monitors
+    
+    def get_monitor_count(self):
+        """Returns total number of monitors."""
+        return sum(len(g.get('monitorList', [])) for g in self.data)
+    
+    def get_stats(self):
+        """Returns summary statistics."""
+        return {
+            'total_groups': len(self.data),
+            'total_monitors': self.get_monitor_count(),
+            'groups': [
+                {
+                    'name': g['name'],
+                    'monitor_count': len(g.get('monitorList', []))
+                }
+                for g in self.data
+            ]
+        }
+
+    def get_monitor_ids_and_names(self):
+        """Returns list of all monitor IDs and names."""
+        result = []
+        for group in self.data:
+            for monitor in group.get('monitorList', []):
+                result.append({
+                    'id': monitor['id'],
+                    'name': monitor['name']
+                })
+        return result
 
 class UptimeKumaClient:
     def __init__(self, url, username, password):
         self.api = UptimeKumaApi(url)
         self.api.login(username, password)
         logging.info(f"Logged into Uptime Kuma at {url}")
+
+    def get_status_page(self, page_name="beacon"):
+        status_page_data = self.api.get_status_page(page_name)
+        return StatusPageParser(status_page_data['publicGroupList'])
+
+    def get_relevant_monitors(self):
+        status_page = self.get_status_page()
+        monitors = status_page.get_monitor_ids_and_names()
+        for monitor in monitors:
+            status_value = self.api.get_monitor_status(monitor['id'])
+            monitor['status'] = MonitorStatus(status_value).name.lower()
+            # logging.info(f"Monitor {monitor['name']} (ID: {monitor['id']}) status: {monitor['status']}")
+        return monitors
